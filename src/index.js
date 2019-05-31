078f858e3bcd7807facf30666268eb2bfb185a1f
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-05-31 14:07:56 +0200

    Introduction to props system

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+    return (
+      <div className="ui container comments">
+        <ApprovalCard>
+            <CommentDetail
+            author="Sam"
+            timeAgo="Today at 4:45PM"
+            avatar={faker.image.avatar()}
+            content="Nice blog Post!"
+            />
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail
+            author="Alex"
+            timeAgo="Today at 12:00PM"
+            avatar={faker.image.avatar()}
+            content="Huh, nice one!"
+            />
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail
+            author="Jane"
+            timeAgo="Yesterday at 5:00PM"
+            avatar={faker.image.avatar()}
+            content="That was awesome!"
+            />
+        </ApprovalCard>
+      </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// semantic-ui.com
+// semantic ui cdn

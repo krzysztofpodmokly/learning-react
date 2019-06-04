30e05dccf538a6a5b6b9fab0d66ef3322cfb453b
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-06-04 10:34:04 +0200

    Resolving merge conflict

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-
-class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        // This is the only time we do direct assignment to THIS.STATE
-        this.state = { lat: null };
-
-        // Connecting to Geolocation API
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // console.log(position);
-                this.setState( { lat: position.coords.latitude } )
-            },
-            (err) => console.log(err)
-        );
-    }
-
-    render() {
-        return <div>Latitude: { this.state.lat }</div>
-    }
-}
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
-=======
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
@@ -73,4 +41,4 @@ ReactDOM.render(<App />, document.querySelector('#root'));
 
 // semantic-ui.com
 // semantic ui cdn
->>>>>>> 078f858e3bcd7807facf30666268eb2bfb185a1f
+

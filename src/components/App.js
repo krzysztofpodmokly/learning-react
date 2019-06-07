b5ed609bfbb45cb32ccedebeae8a62c2f9216827
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-06-07 16:23:14 +0200

    Intro to Redux

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { selectSong } from '../actions'; // importing index.js from actions dir
+
+const App = () => {
+    return <div>App</div>
+}
+
+export default App;

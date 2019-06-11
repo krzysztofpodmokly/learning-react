0406b24f1f398e9d6242c227f8cdeccd19f895aa
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-06-11 12:28:10 +0200

    Removing unwanted comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,18 +27,8 @@ export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
-    // Bad Approach!!!
-    // With this approach we are receiving either API response or an object
-    // const response = await jsonPlaceholder.get('/posts');
-
-    // Redux-Thunk allows for returning either action object or function
-    // and if function is returned Thunk invokes this function automatically with 
-    // dispatch & getState as arguments
-    
 };
 
-// Lodash library is used here to prevent from sending multiple requests
-// with _.memoize function is save in memory once it was invoked for the first time
 export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response });

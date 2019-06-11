8422f87e1770b44569c279c4ef8aa790aa5672aa
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-06-11 12:24:35 +0200

    Other overfetch approach with _.chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,27 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    // console.log('About to fetch posts')
+    await dispatch(fetchPosts());
+
+    // METHOD 1
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // METHOD 2
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value(); // execute
+
+    // await Promise.all(userIds.map(id => dispatch(fetchUser(id))));
+    // console.log(userIds)
+    // console.log('getState() => ', getState().posts);
+    // console.log('Fetched posts')
+};
+
 // Action creator
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -18,12 +39,11 @@ export const fetchPosts = () => async (dispatch) => {
 
 // Lodash library is used here to prevent from sending multiple requests
 // with _.memoize function is save in memory once it was invoked for the first time
-export const fetchUser = (id) => dispatch => _fetchuser(id, dispatch);
-
-const _fetchuser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response });
-});
+};
+
 
 // export const fetchUser = function(id) { 
 //     return async function(dispatch) {
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
 
     render() {
         // Outsourcing finding a proper user to mapStateToProps
@@ -28,4 +24,4 @@ const mapStateToProps = (state, ownProps) => {
     return { user: state.users.find(user => user.data.id === ownProps.userId) }
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
